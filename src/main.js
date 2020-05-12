90209abd10249e47d98cbcb1752c44fd2ad1e36b
Tamires-Tenorio <tamirestenorio03@gmail.com>
2020-05-12 16:41:01 -0300

    commiti

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import { pokemon } from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let figura = document.querySelector(".pokemonFigura");
+
+//let retornoPokemon = pokemon(data);
+//figura.innerHTML=retornoPokemon.join("<br/> ") ;
+for (let pokemonTotal in data.pokemon){
+  // figura.innerHTML+='<img src="'+retornoPokemon[imagem]+'"/>'
+  let img = document.createElement("img")
+  let name = document.createElement("p")
+  let type = document.createElement("p")
+  let div = document.createElement("div")
+  name.innerHTML=data.pokemon[pokemonTotal].name
+  type.innerHTML=data.pokemon[pokemonTotal].type
+  img.setAttribute('src',data.pokemon[pokemonTotal].img)
+  div.classList.add("cardPokemon")
+  div.appendChild(name)
+  div.appendChild(type)
+  div.appendChild(img)
+  figura.appendChild(div)
+
+}
+
+console.log();
