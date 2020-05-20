e80705478fe5f30589a6b2c4e940b382ecf2d53e
francielisabreu <contato.theframboesa@gmail.com>
2020-05-20 12:34:46 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
-import { filteByName, sortBy } from './data.js';
-import data from './data/pokemon/pokemon.js';
+import { pokemon } from "./data.js";
+import data from "./data/pokemon/pokemon.js";
 
-const searchPokemon = document.querySelector("#inputForSearchGeneral");
-const pokemon = data.pokemon
-const pokemonByOrder = document.querySelector("#labelForSelectOrder");
-const pokemonCard = document.getElementById("pokemonCard")
+let figure = document.querySelector(".pokemonFigure");
 
-const
+//let retornoPokemon = pokemon(data);
+//figura.innerHTML=retornoPokemon.join("<br/> ") ;
+for (let pokemonTotal in data.pokemon) {
+  // figura.innerHTML+='<img src="'+retornoPokemon[imagem]+'"/>'
+  let img = document.createElement("img");
+  let name = document.createElement("p");
+  let type = document.createElement("p");
+  let num = document.createElement("p");
+  let div = document.createElement("div");
+  name.innerHTML = data.pokemon[pokemonTotal].name;
+  type.innerHTML = data.pokemon[pokemonTotal].type;
+  num.innerHTML = data.pokemon[pokemonTotal].num;
+  img.setAttribute("src", data.pokemon[pokemonTotal].img);
+  div.classList.add("cardPokemon");
+  div.appendChild(name);
+  div.appendChild(type);
+  div.appendChild(num);
+  div.appendChild(img);
+  figure.appendChild(div);
+}
 
+const cardPokemon = document.querySelector(".cardPokemon");
+cardPokemon.addEventListener("click", () => openModal("modal"));
+
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.classList.add("show");
+  modal.addEventListener("click", (evento) => {
+    if (
+      evento.target.id === modalId ||
+      evento.target.className === "close cardPokemon"
+    ) {
+      modal.classList.remove("show");
+    }
+  });
+}
